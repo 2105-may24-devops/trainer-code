8d079937da4225e9ddb68da25e7ba8daf735d2cf
Nicholas Escalona <nicholas.escalona@gmail.com>
2021-05-27 11:33:10 -0500

    add transpose soln

---
+++ 1-python/python/transpose.py
@@ -0,0 +1,46 @@
+# exercism exercise "transpose"
+def transpose(lines: str) -> str:
+    input_list = lines.split('\n') # or splitlines
+    input_height = len(input_list)
+    input_width = get_input_width(input_list)
+
+    output_list = []
+
+    for colnum in range(input_width):
+        output = ''
+        for rownum in range(input_height):
+            row = input_list[rownum]
+            if colnum >= len(row):
+                char = '*'
+            else:
+                char = row[colnum]
+            output += char
+        output = output.rstrip('*').replace('*', ' ')
+        output_list.append(output)
+
+    return '\n'.join(output_list)
+
+def get_input_width(input_list):
+    # max_length = 0
+    # for i in range(len(input_list)):
+    #     row = input_list[i]
+    #     if len(row) > max_length:
+    #         max_length = len(row)
+
+    # max_length = 0
+    # for row in input_list:
+    #     if len(row) > max_length:
+    #         max_length = len(row)
+
+    # list comprehension
+    # lengths = [len(x) for x in input_list] # list of ints
+    # max_length = max(lengths)
+
+    # generator expression
+    # an expression that acts like a sequence, that's not built yet
+    max_length = max(len(x) for x in input_list, default=0)
+
+    return max_length
+
+print(transpose("AB\nC"))
+print(repr(transpose("AB\nC").split('\n')))
